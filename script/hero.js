414b43726da4d23d5791cd13609dfacc10dc1854
Daniel-Georgiev <daniko91@mail.bg>
2014-11-17 15:53:24 +0200

    player cannot go outside borders

--- script/hero.js
+++ script/hero.js
@@ -1,3 +1,5 @@
+var keys = [false, false];
+
 function Player(){
     this.x = 150;
     this.y = 350;
@@ -7,23 +9,27 @@ function Player(){
         ctx.drawImage(nerd,this.x,this.y);
 
     };
+
     document.addEventListener('keydown', function(event){
         var key_press = event.keyCode;
         //alert(key_press);
-        if(key_press == "37"){
+
+        if((key_press == "37")&&(player.x>-20)){
+//            keys[0] = true;
             player.x-=10;
-
-
         }
-        if(key_press == "39"){
+
+//        if(keys[0]){
+//            player.x-=6;
+//        }
+        if((key_press == "39")&&(player.x<290)){
+//            keys[1] = true;
             player.x+=10;
-
         }
+//        if(keys[1]){
+//            player.x+=6;
+//        }
+
     })
-
-
-
-
-
 
 }
