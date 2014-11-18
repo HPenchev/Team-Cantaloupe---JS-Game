daa42520d06103ca1c34000fc5cf8056db0d1d7f
Peter-Koev <peter_bs94@abv.bg>
2014-11-18 19:40:18 +0200

    Bullets are now bits.

--- script/bullets.js
+++ script/bullets.js
@@ -12,21 +12,33 @@ function bulletsComing(){
             Bullet();
         }
     });
-
+    var bit0 = new Image();
+    bit0.src = "../img/bit0.png";
+    var bit1 = new Image();
+    bit1.src = "../img/bit1.png";
 
     for(var i = 0; i<bullets.length; i++){
-        ctx.fillRect(bullets[i].x, bullets[i].y, bullets[i].w, bullets[i].h);
+        var bitChoice = Math.floor(Math.random() * 2);
+        if(bitChoice){
+            ctx.drawImage(bit1, bullets[i].x, bullets[i].y);
+        } else{
+            ctx.drawImage(bit0, bullets[i].x, bullets[i].y);
+
+        }
+        //ctx.fillRect(bullets[i].x, bullets[i].y, bullets[i].w, bullets[i].h);
         bullets[i].y-=10;
         this.hitDetection(bullets[i], i);
         if(bullets[i].y < -20){
             bullets.splice(i, 1);
         }
     }
+
     this.hitDetection = function(bullet, pos){
         for(var j = 0; j < enemies.length; j++){
             if(enemies[j].x <= bullet.x - bullet.w && enemies[j].x + 2 * enemies[j].w>= bullet.x &&
                 enemies[j].y + enemies[j].h >= bullet.y - 3 * bullet.h && enemies[j].y + enemies[j].h <= bullet.y){
                 bullets.splice(pos, 1);
+
                 enemies.splice(j, 1);
             }
         }
